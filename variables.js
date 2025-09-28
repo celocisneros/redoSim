975725cc892be687a83b839373a3fbc59c36b2e2
celocisneros <celo.cisneros@icloud.com>
2025-09-28 12:06:57 +0000

    Add day display and style dayBox for better visibility

--- variables.js
+++ variables.js
@@ -9,3 +9,15 @@ let timeDisplay = `Time: ${time}`;
 
 let energy = 100;
 let energyDisplay = `Energy: ${energy}`;
+
+let hunger = 0;
+let hungerDisplay = `Hunger: ${hunger}`;
+
+let inventory = [];
+let inventoryDisplay = `Inventory: ${inventory.length} items`;
+
+let location = "home";
+let locationDisplay = `Location: ${location}`;
+
+let job = null;
+let jobDisplay = `Job: ${job ? job : "Unemployed"}`;
