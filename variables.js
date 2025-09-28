3d69e640a7ea24288aface90bde2706cd1865d3c
celocisneros <celo.cisneros@icloud.com>
2025-09-28 11:49:03 +0000

    Enhance game display by adding day, time, energy, job, house, hunger, and quest elements; update cash box styling for better visibility.

--- variables.js
+++ variables.js
@@ -1,2 +1,11 @@
 let cash = 0;
 let cashDisplay = `$${cash}`;
+
+let day = 0;
+let dayDisplay = `Day: ${day}`;
+
+let time = "day";
+let timeDisplay = `Time: ${time}`;
+
+let energy = 100;
+let energyDisplay = `Energy: ${energy}`;
