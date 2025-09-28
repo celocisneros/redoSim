2102069233b05b2f9d9cdb06efffb92baddc2c4d
celocisneros <celo.cisneros@icloud.com>
2025-09-28 14:48:33 +0000

    Implement code changes to enhance functionality and improve performance

--- functions.js
+++ functions.js
@@ -11,15 +11,38 @@ function updateDisplays(){
     document.getElementById("day").innerText = `Day: ${day}`;
     document.getElementById("time").innerText = `Time: ${time}`;
 }
-
+function updatePhonePerson(imagePath){
+    document.getElementById("person").style.backgroundImage = `url('${imagePath}')`;
+    document.getElementById("person").style.backgroundSize = "contain";
+    document.getElementById("person").style.backgroundPosition = "center";
+    document.getElementById("person").style.backgroundRepeat = "no-repeat";
+}
 function changeBackground(imagePath){
     document.body.style.backgroundImage = `url('${imagePath}')`;
     document.body.style.backgroundSize = "70%";
     document.body.style.backgroundPosition = "center";
     document.body.style.backgroundRepeat = "no-repeat";
 }
+function phoneCallSound(){
+  const audio = new Audio('ring1.mp3');
+  audio.volume = 0.5; // Set volume
+  audio.play();
+}
 function playBackgroundMusic(filePath){
   const audio = new Audio(filePath);
+  audio.volume = 0.5; // Set volume
+  audio.play();
+}
+function displayPhone(){
+    document.getElementById("iphone").style.display="block";
+}
+function phoneCallChef(){
+    displayPhone();
+    updatePhonePerson('oldChef.jpg');
+    document.getElementById("screen").innerText = "Chef";
+}
+function playLoadingSound(){
+  const audio = new Audio('loading1.mp3');
   audio.volume = 0.5; // Set volume
   audio.play();
 }
@@ -31,6 +54,8 @@ function startGame(){
     document.getElementById("restaraunt").style.display="flex";
     document.getElementById("dayBox").style.display="flex";
     document.getElementById("timeBox").style.display="flex";
+    document.getElementById("howToPlay").style.display="block";
     changeBackground('night1.jpg');
     updateDisplays();
+    playLoadingSound();
 } 
