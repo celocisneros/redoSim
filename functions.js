ca126d2a171f607f6d2b9b0162da2e8d1d7ce1fe
celocisneros <celo.cisneros@icloud.com>
2025-09-28 02:14:37 +0000

    Implement code changes to enhance functionality and improve performance

--- functions.js
+++ functions.js
@@ -3,3 +3,14 @@ function playBackgroundMusic(filePath){
   audio.volume = 0.5; // Set volume
   audio.play();
 }
+function startGame(){
+    playBackgroundMusic(`japanLofiCut.mp3`);
+    document.getElementById("welcomeMessage").style.display="none";
+    document.getElementById("startButton").style.display="none";
+    document.getElementById("cashBox").style.display="flex";
+    document.getElementById("restaraunt").style.display="flex";
+    document.body.style.backgroundImage = "url('day1.jpg')";
+    document.body.style.backgroundSize = "70%";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
