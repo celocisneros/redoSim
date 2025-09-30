1789c3d9973247d5b8374274c941d9ba219073d2
celocisneros <celo.cisneros@icloud.com>
2025-09-30 01:35:37 +0000

    Refactor game structure by removing unused elements and enhancing player stats display; update game initialization process.

--- functions.js
+++ functions.js
@@ -1,42 +1,17 @@
 //define game logic variables
 let cash = 100;
-let cashDisplay = `$${cash}`;
-let day = 1;
-let time = "Night";
+let cashDisplay = `💵 $${cash}`;
+let job = "Dishwasher";
+let jobDisplay = `👔 ${job}`;
 
-
-function playLoadingSound(){
-  const audio = new Audio('loading1.mp3');
-  audio.volume = 1.0; // Set volume
-  audio.play();
-}
-function changeBackground(imagePath){
-    document.body.style.backgroundImage = `url('${imagePath}')`;
-    document.body.style.backgroundSize = "70%";
-    document.body.style.backgroundPosition = "center";
-    document.body.style.backgroundRepeat = "no-repeat";
-}
-function playBackgroundMusic(filePath){
-  const audio = new Audio(filePath);
-  audio.volume = 0.5; // Set volume
-  audio.play();
-}
-function updateDisplays(){
-    document.getElementById("cash").innerText = cashDisplay;
-    document.getElementById("day").innerText = `Day: ${day}`;
-    document.getElementById("time").innerText = `Time: ${time}`;
-}
 function startGame(){
-    playBackgroundMusic(`japanLofiCut.mp3`);
-    document.getElementById("welcomeMessage").style.display="none";
-    document.getElementById("startButton").style.display="none";
-    document.getElementById("cashBox").style.display="flex";
-    document.getElementById("restaraunt").style.display="flex";
-    document.getElementById("dayBox").style.display="flex";
-    document.getElementById("timeBox").style.display="flex";
-    document.getElementById("howToPlay").style.display="block";
-    changeBackground('night1.jpg');
-    updateDisplays();
-    playLoadingSound();
-} 
-
+  document.getElementById('welcomeMessage').style.display = "none";
+  document.getElementById('welcomeButton').style.display = "none";
+  document.getElementById('map').style.display = "flex";
+  document.getElementById('playerStats').style.display = "flex";
+}
+function updateStats(){
+  document.getElementById('cash').innerText = cashDisplay;
+  document.getElementById('job').innerText = jobDisplay;
+}
+updateStats();
